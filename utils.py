55936d11f2f18d448e87f38ed46210356944dcf8
Kim Jimin <kimjimin1507@gmail.com>
2026-01-31 22:10:51 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,6 @@ class_list = ["empty", "person", "vehicle"]
 # shipWatcher model architecture
 # Initialise ResNet-18 with pre-trained weights as a feature extractor
 # Modify full connected(fc) layer with nn.Linear for 3-class classification
-
 def get_shipWatcher():
     model = models.resnet18(weights="DEFAULT")
     for param in model.parameters():
